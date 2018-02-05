e0aa9052d411ee0be62d4b41ec276c0af8810163
Mehdi Drissi <mdrissi@hmc.edu>
2018-02-05 14:16:33 -0800

    committing random stuff

---
+++ PrettyMuchBlank/app/src/main/java/com/example/mehdi2277/prettymuchblank/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mehdi2277.prettymuchblank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
