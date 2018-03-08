8836282bf8742612fb9fdaa466e567d9c18cab44
Mehdi Drissi <mdrissi@cs.hmc.edu>
2018-03-07 21:06:05 -0800

    Finished lambda interp and some modifications to the parsers

--- ANC/ForInterp.hs
+++ ANC/ForInterp.hs
@@ -5,25 +5,39 @@ import ForParser
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Function
 import Control.Applicative (liftA2)
 
 type Context = Map String Int
 
 evalExpr :: Context -> Expr -> Maybe Int
-evalExpr store (Var str) = Map.lookup store str
+evalExpr store (Var str) = Map.lookup str store
 evalExpr store (Const i) = Just i
 evalExpr store (Plus expr1 expr2) = liftA2 (+) (evalExpr store expr1) (evalExpr store expr2)
 evalExpr store (Minus expr1 expr2) = liftA2 (-) (evalExpr store expr1) (evalExpr store expr2)
 
 evalCmp :: Context -> Cmp -> Maybe Bool
 evalCmp store (Equal expr1 expr2) = liftA2 (==) (evalExpr store expr1) (evalExpr store expr2)
-evalCmp store (Le expr1 expr2) = liftA2 (==) (evalExpr store expr1) (evalExpr store expr2)
-evalCmp store (Ge expr1 expr2) = liftA2 (==) (evalExpr store expr1) (evalExpr store expr2)
+evalCmp store (Le expr1 expr2) = liftA2 (<) (evalExpr store expr1) (evalExpr store expr2)
+evalCmp store (Ge expr1 expr2) = liftA2 (>) (evalExpr store expr1) (evalExpr store expr2)
 
+evalHelper :: Context -> ProgFor -> Maybe Context
+evalHelper store prog@(For var _ cond e2 body) = do cond_value <- evalCmp store cond
+                                                    if cond_value
+                                                        then do store' <- eval store (Seq body (Assign var e2))
+                                                                evalHelper store' prog
+                                                        else return store
+evalHelper store prog = eval store prog
 
 eval :: Context -> ProgFor -> Maybe Context
-eval store (Assign str expr) = store.insert str expr
+eval store (Assign str expr) = flip (Map.insert str) store <$> evalExpr store expr
 eval store (If c prog1 prog2) = case (evalCmp store c) of
-	Just True -> eval store prog1
-	Just False -> eval store prog2
-	_ -> Nothing
+                                    Just True -> eval store prog1
+                                    Just False -> eval store prog2
+                                    _ -> Nothing
+eval store prog@(For var e1 _ _ _) = do store' <- eval store (Assign var e1)
+                                        evalHelper store' prog
+eval store (Seq prog1 prog2) = do store' <- eval store prog1
+                                  eval store' prog2
--- ANC/ForLambdaCommon.hs
+++ ANC/ForLambdaCommon.hs
@@ -13,12 +13,17 @@ import Control.Applicative (liftA2)
 import Data.Aeson
 import GHC.Generics
 
-data Expr = Var String | Const Integer | Plus Expr Expr | Minus Expr Expr deriving Generic
+data Expr = Var String | Const Int | Plus Expr Expr | Minus Expr Expr deriving Generic
 data Cmp = Equal Expr Expr | Le Expr Expr | Ge Expr Expr deriving Generic
+
+is_value :: Expr -> Bool
+is_value (Var _) = True
+is_value (Const _) = True
+is_value _ = False
 
 showExpr :: Int -> Expr -> String
 showExpr 0 (Plus x y) = (showExpr 0 x) ++ " + " ++ (showExpr 1 y)
-showExpr 0 (Minus x y) = (showExpr 0 x) ++ " + " ++ (showExpr 1 y)
+showExpr 0 (Minus x y) = (showExpr 0 x) ++ " - " ++ (showExpr 1 y)
 showExpr 0 a = showExpr 1 a
 
 showExpr 1 (Var x) = x
@@ -42,3 +47,59 @@ instance ToJSON Cmp where
     toEncoding = genericToEncoding defaultOptions
 
 instance FromJSON Cmp
+
+sharedDef :: P.LanguageDef ()
+sharedDef = emptyDef {P.identStart = letter,
+                      P.identLetter = alphaNum,
+                      P.opStart = P.opLetter sharedDef,
+                      P.opLetter = oneOf "+-=;><",
+                      P.reservedOpNames = ["=", "-", "==", "+", ">", ";", "<"],
+                      P.reservedNames = ["if", "then", "else", "endif", "for", "do", "endfor", "let", "letrec",
+                                          "in", "unit"]}
+
+lexer :: P.TokenParser ()
+lexer = P.makeTokenParser sharedDef
+
+reserved :: String -> Parser ()
+reserved = P.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = P.reserved lexer
+
+double_equal :: Parser ()
+double_equal = P.reservedOp lexer "=="
+
+equal :: Parser ()
+equal = P.reservedOp lexer "="
+
+semicolon :: Parser ()
+semicolon = P.reservedOp lexer ";"
+
+plus :: Parser ()
+plus = P.reservedOp lexer "+"
+
+minus :: Parser ()
+minus = P.reservedOp lexer "-"
+
+le :: Parser ()
+le = P.reservedOp lexer "<"
+
+ge :: Parser ()
+ge = P.reservedOp lexer ">"
+
+identifier :: Parser String
+identifier = P.identifier lexer
+
+parens :: Parser a -> Parser a
+parens = P.parens lexer
+
+expr, expr_term, var, constant :: Parser Expr
+expr = expr_term `chainl1` ((plus *> pure Plus) <|> (minus *> pure Minus))
+expr_term = var <|> constant <|> parens expr
+var = Var <$> identifier
+constant = Const <$> (fromInteger <$> P.integer lexer)
+
+cmp :: Parser Cmp
+cmp = try (liftA2 Equal (expr <* double_equal) expr) <|>
+       try (liftA2 Le (expr <* le) expr) <|>
+       liftA2 (Ge) (expr <* ge) expr <|> parens cmp
