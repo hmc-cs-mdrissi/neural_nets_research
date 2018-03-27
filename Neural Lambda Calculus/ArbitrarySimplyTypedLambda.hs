9f771f041d19c7195f241bdda500999446fb859e
Pedro Sandoval <psandovalsegura@gmail.com>
2018-03-27 09:32:42 -0700

    Add a few simple cases to lc gen

--- Neural Lambda Calculus/ArbitrarySimplyTypedLambda.hs
+++ Neural Lambda Calculus/ArbitrarySimplyTypedLambda.hs
@@ -1,27 +1,33 @@
+{-# LANGUAGE TupleSections #-}
+
 module ArbitrarySimplyTypedLambda where
 
 import SimplyTypedLambdaParser
 import Test.QuickCheck
+import Control.Applicative (liftA2)
 
 -- 
 -- ArbitrarySimplyTypedLambda.hs
 -- A set of functions for generating arbitrary simply typed lambda calculus programs.
 -- 
 
-import Data.Map (Map)
+import Data.Map (Map, toList, member)
 import qualified Data.Map as Map
 
 type Context = Map String Type
 
-combineStringWithNumber :: String -> Int -> String
-combineStringWithNumber s i = s ++ show i
+isTypeVariablePair :: Type -> (Type, b) -> Bool
+isTypeVariablePair typ (typ', b) = if typ == typ' then True else False
 
-arbitraryIdentifier :: Int -> Gen String
-arbitraryIdentifier upperBound = do identifier <- elements $ map (combineStringWithNumber "a") [1 .. upperBound]
-                                    return identifier
+freshVariableGivenContext :: Context -> Gen String
+freshVariableGivenContext context = return $ freshVariableAux 1 context
+                                    where freshVariableAux n context = if ("a" ++ show n) `member` context then freshVariableAux (n + 1) context else "a" ++ show n
+
+getArbitraryIdentifierFromContext :: Context -> Type -> Gen String
+getArbitraryIdentifierFromContext context typ = elements $ map fst $ filter ((==typ) . snd) $ (toList context)
 
 arbitraryConstant :: Gen Integer
-arbitraryConstant = do firstInt <- elements $ [1 .. 10]
+arbitraryConstant = do firstInt <- elements $ [0 .. 10]
                        return firstInt
 
 --data LambdaExpression = 
@@ -49,23 +55,27 @@ arbitraryConstant = do firstInt <- elements $ [1 .. 10]
 -- Type
 
 arbitraryType :: Int -> Gen Type
-arbitraryType n | n <= 0 = frequency [(1, return TInt), (1, return TBool), (1, return TIntList), (1, return TFake)]
+arbitraryType n | n <= 0 = frequency [(1, return TInt), (1, return TBool), (1, return TIntList)]
                 | otherwise = TFun <$> smallerType <*> smallerType where smallerType = arbitraryType (n `div` 2)
-
-arbitraryNumber :: Gen LambdaExpression
-arbitraryNumber = Number <$> arbitraryConstant
 
 arbitraryBinaryOpArithmetic :: Gen BinaryOp
 arbitraryBinaryOpArithmetic = frequency [(1, return Plus), (1, return Minus), (1, return Times), (1, return Divide)]
 
--- < fix
 arbitraryBinaryOpLogical :: Gen BinaryOp
-arbitraryBinaryOpLogical = frequency [(1, return And), (1, return Or), (1, return Equal)]
+arbitraryBinaryOpLogical = frequency [(1, return And), (1, return Or)]
 
-arbitraryBoolean :: Int -> Context -> Gen LambdaExpression
-arbitraryBoolean n context | n <= 0 = Boolean <$> (elements [True, False])
-                           | otherwise = frequency [(1, BinaryOper <$> smallerBoolean <*> arbitraryBinaryOpLogical <*> smallerBoolean)]
-                                         where smallerBoolean = arbitraryBoolean (n - 1) context
+keepContext :: Monad m => Context -> m a -> m (a, Context)
+keepContext context ma = do a <- ma
+                            return (a, context)
+
+arbitraryVariable :: Context -> Gen (LambdaExpression, Context)
+arbitraryVariable context = keepContext context (Variable <$> freshVariableGivenContext context)
+
+arbitraryNumber :: Context -> Gen (LambdaExpression, Context)
+arbitraryNumber context = keepContext context (Number <$> arbitraryConstant)
+
+arbitraryBoolean :: Context -> Gen (LambdaExpression, Context)
+arbitraryBoolean context = keepContext context (Boolean <$> (elements [True, False])) 
 
 --arbitraryNumberList :: Int -> Context -> Gen LambdaExpression
 --arbitraryNumberList n context = undefined
@@ -76,6 +86,3 @@ arbitraryBoolean n context | n <= 0 = Boolean <$> (elements [True, False])
 --arbitraryLambda :: Int -> Context -> Gen LambdaExpression
 --arbitraryLambda n context = undefined
 
----- Return the variables in the context that 
---findType :: Type -> Context -> [String]
---findType type1 context = undefined 
