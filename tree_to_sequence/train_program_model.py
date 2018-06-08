05ccb0e420952265d29cd35ff89f1c81bca50631
Olivia Watkins <oliviagracewatkins@gmail.com>
2018-06-08 07:29:39 -0700

    make js->cs run

--- tree_to_sequence/train_program_model.py
+++ tree_to_sequence/train_program_model.py
@@ -139,8 +139,8 @@ if opt.problem_number == 0:
     parent_to_category = partial(parent_to_category_LAMBDA, num_vars, num_ints)
     category_to_child = partial(category_to_child_LAMBDA, num_vars, num_ints)
 else:
-    encoder_input_size = num_vars + num_ints + len(for_ops)
-    nclass = num_vars + num_ints + len(lambda_ops)
+    encoder_input_size = num_vars + num_ints + len(javascript_ops)
+    nclass = num_vars + num_ints + len(coffee_ops)
     num_categories = len(CoffeeGrammar)
     num_possible_parents = len(Coffee)
     max_num_children = 2 if binarize_output else 3
